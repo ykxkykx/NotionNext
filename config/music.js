c0dabba723c1c37342eb6ee34526225cf8f7b5d4
ykxkykx <ykxkykx@foxmail.com>
2023-01-04 01:48:43 +0800

    fix: 轨迹纯音乐歌单扩充

---
+++ config/music.js
@@ -0,0 +1,50 @@
+const musicUrl = 'https://music.163.com/song/media/outer/url?id='
+
+const MUSIC_PLAYER_AUDIO_LIST = [
+  {
+    name: '风を共に舞う気持ち',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '731419.mp3',
+    cover: 'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+  },
+  {
+    name: '王都グランセル',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '731355.mp3',
+    cover: 'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+  },
+  {
+    name: '工房都市ツァイス',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '731333.mp3',
+    cover: 'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+  },
+  {
+    name: '空を见上げて',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '730635.mp3',
+    cover: 'https://p2.music.126.net/-D5kuypDn312GzKW2Q2Ydg==/566248488352545.jpg'
+  },
+  {
+    name: 'I swear...',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '730633.mp3',
+    cover: 'https://p1.music.126.net/-D5kuypDn312GzKW2Q2Ydg==/566248488352545.jpg'
+  },
+  {
+    name: '海港都市ルーアン',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '731291.mp3',
+    cover: 'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+  },
+  {
+    name: 'LOST HARMONY AMONG PEOPLE',
+    artist: 'Falcom Sound Team jdk',
+    url: musicUrl + '729957.mp3',
+    cover: 'https://p1.music.126.net/vAVkm3vd_TLiBukkJBsm7w==/624522604624169.jpg'
+  }
+]
+
+module.exports = {
+  MUSIC_PLAYER_AUDIO_LIST
+}
